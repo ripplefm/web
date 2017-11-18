2bd7fa70bf80258af5630f19b9b8e3f375bdc4d3
Daniel <danielrudn@gmail.com>
2017-11-18 00:59:10 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './js/components/app';
+import store from './js/store';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
