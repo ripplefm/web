beafd18612695ff415bc6fd4ab4ec34429898374
Daniel Rudnitski <danielrudn@gmail.com>
2019-02-27 17:58:08 -0500

    Improve development experience (#8)
    * Update 'ripple.fm' library to 0.1.2
    
    * Add dev dockerfile and docker-compose config for other ripple.fm services
    
    * Add README with documentation
    
    * Add CHANGELOG.md and bump version to 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.1.1] - 2019-02-27
+
+### Added
+
+- Added `docker-compose.yaml` to configure other ripple services to start locally for a better development experience
+- Added `README.md` and `CHANGELOG.md`
+
+## [0.1.0] - 2019-02-16
+
+### Added
+
+- Configuration for Travis CI to run and build production docker image on tagged commits
---
+++ support/station_templates.exs
@@ -0,0 +1,96 @@
+# This file is used to define preset stations that
+# are automatically started and have auto player servers
+# started to play tracks from the sources.
+# You should return an array of station templates, each must be a map
+# with the following keys: name, tags, sources
+# The sources map can have the keys ["playlist", "user", "track", "related"]
+# Each source key can map to either a string or a list of strings
+
+station_templates = [
+  %{
+    name: "Popular Hits",
+    tags: [],
+    sources: %{
+      playlist: [
+        "https://youtube.com/playlist?list=PL4fGSI1pDJn4IeWA7bBJYh__qgOCRMkIh",
+        "https://youtube.com/playlist?list=PL4fGSI1pDJn5kI81J1fYWK5eZRl1zJ5kM"
+      ]
+    }
+  },
+  %{
+    name: "Bubble POP!",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_lGEOjy5U8xV41C8_LyqNnAZKOH6sGyutI"
+    }
+  },
+  %{
+    name: "Happy Hip Hop",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_mkEwQuegHYB8_aAzBO8Q__6gGoaFblISw"
+    }
+  },
+  %{
+    name: "edm hits",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_npkiQvDU863KxBgX_eaYo-LCIuM3qZtqo"
+    }
+  },
+  %{
+    name: "classic pop",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_k4sXYMRc7kePx3BjjVv5z2fB1CePqvVDw"
+    }
+  },
+  %{
+    name: "next up",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_nPY2b4tMzpmW2HPDvr8YgN9BGu8TBDgAU"
+    }
+  },
+  %{
+    name: "popular hip hop",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_kNzdtZz5yF-crDDcYxlIDOju50Tyqnrig"
+    }
+  },
+  %{
+    name: "party time",
+    tags: [],
+    sources: %{
+      playlist: [
+        "https://youtube.com/playlist?list=RDCLAK5uy_l3XabTm3_hzKqB9XOpyJj2xYRO597H5eE",
+        "https://youtube.com/playlist?list=RDCLAK5uy_nfs_t4FUu00E5ED6lveEBBX1VMYe1mFjk"
+      ]
+    }
+  },
+  %{
+    name: "mellow",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_mPolD_J22gS1SKxufARWcTZd1UrAH_0ZI"
+    }
+  },
+  %{
+    name: "throwbacks",
+    tags: [],
+    sources: %{
+      playlist: [
+        "https://youtube.com/playlist?list=RDCLAK5uy_kN4eY_ibobGvCBwIJGEGpDjuwzYHIG_iE",
+        "https://youtube.com/playlist?list=RDCLAK5uy_nvu_HUY0DPNpbB_V0moUfs9s5zABAF8xQ"
+      ]
+    }
+  },
+  %{
+    name: "old country",
+    tags: [],
+    sources: %{
+      playlist: "https://youtube.com/playlist?list=RDCLAK5uy_kpxnNxJpPZjLKbL9WgvrPuErWkUxMP6x4"
+    }
+  }
+]
