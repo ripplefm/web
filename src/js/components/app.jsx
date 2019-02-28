f588bfd95150afb40fe1f2574fe83f49d80d70fd
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-02-27 20:53:25 -0500

    Replace homepage to show landing page when guest and dashboard when logged in

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { withRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { init } from '../actions/oauth-actions';
-import Home from './home/home';
+import LandingPage from './landing-page';
+import Dashboard from './dashboard';
 import Stations from './stations/stations';
 import '../../css/common/common.css';
+
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.user !== undefined
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -18,11 +25,22 @@ class App extends Component {
   }
 
   render() {
+    const { loggedIn } = this.props;
     return [
-      <Route key="home" path="/" exact component={Home} />,
+      <Route
+        key="home"
+        path="/"
+        exact
+        component={loggedIn ? Dashboard : LandingPage}
+      />,
       <Route key="stations" path="/stations" component={Stations} />
     ];
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
