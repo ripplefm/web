dc39d6360f0cb6616741255dd601c7519aa4cf3a
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-03-20 09:48:42 -0400

    Add actions for retrieving stations user follows

--- src/js/reducers/stations-reducer.js
+++ src/js/reducers/stations-reducer.js
@@ -2,6 +2,7 @@ import { actions } from '../actions/stations-actions';
 
 const initialState = {
   stations: [],
+  followingStations: [],
   loading: false,
   error: undefined
 };
@@ -14,6 +15,12 @@ export default function(state = initialState, action) {
       return { ...state, loading: false, stations: action.stations.stations };
     case actions.REQUEST_STATIONS_ERROR:
       return { ...state, loading: false, error: action.error };
+    case actions.REQUEST_FOLLOWING_STATIONS:
+      return { ...state, followingStations: [] };
+    case actions.RECEIVE_FOLLOWING_STATIONS:
+      return { ...state, followingStations: action.stations.stations };
+    case actions.REQUEST_FOLLOWING_STATIONS_ERROR:
+      return { ...state, followingStations: [], error: action.error };
     default:
       return state;
   }
