28bf876154f6d771bbc34bfee476de935dba6996
Daniel Rudnitski <danielrudn@gmail.com>
2019-03-20 10:09:40 -0400

    Remove unfinished MyStations component (#12)
    * Routes only show dashboard or landing page now
    
    * Update auth service to 0.2.1
    
    * Remove unused components
    
    * Add actions for retrieving stations user follows
    
    * Add static json file for featured stations
    
    * Add dashboard home page with featured stations banner
    
    * Minor refactor for landing page
    
    * Add banners for featured stations
    
    * Bump to 0.2.0 and update changelog
    
    * Remove unfinished MyStations component

--- src/js/components/dashboard/index.jsx
+++ src/js/components/dashboard/index.jsx
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router';
 import { Layout } from 'antd';
 import DashboardHome from './pages/dashboard-home';
 import DashboardMenu from './dashboard-menu';
-import MyStations from './pages/my-stations';
 
 export default class Dashboard extends Component {
   render() {
@@ -14,7 +13,6 @@ export default class Dashboard extends Component {
           <Layout.Content style={{ background: '#212121' }}>
             <Switch>
               <Route exact path="/" component={DashboardHome} />
-              <Route exact path="/my-stations" component={MyStations} />
             </Switch>
           </Layout.Content>
         </Layout>
