863c30165ae7ca00d53a4062b8991fa9125ad41c
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-03-28 12:59:33 -0400

    Update index page to show landing page or dashboard when authenticated

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import LandingPage from '../components/landing-page';
+import Dashboard from '../components/dashboard';
+import { getOrCreate } from '../lib/services/ripple-api';
+import wrapUnauthorized from '../lib/utils/wrap-unauthorized';
 
 export default class Index extends Component {
-  static async getInitialProps() {
-    return {};
+  static async getInitialProps(context) {
+    const ripple = await getOrCreate(context);
+
+    const results = await Promise.all([
+      ripple.getStations(),
+      wrapUnauthorized(ripple.getCurrentUser()),
+      wrapUnauthorized(ripple.getFollowedStations())
+    ]);
+
+    return {
+      stations: results[0] ? results[0].stations : [],
+      user: results[1],
+      followingStations: results[2] ? results[2].stations : []
+    };
   }
 
   render() {
-    return (
-      <div>
-        <h1>index</h1>
-      </div>
-    );
+    const { user, stations, followingStations } = this.props;
+
+    if (user) {
+      return (
+        <Dashboard
+          user={user}
+          popularStations={stations}
+          followingStations={followingStations}
+        />
+      );
+    }
+    return <LandingPage stations={stations} />;
   }
 }
