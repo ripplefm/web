ed50a0e983742360c0ca55e1c35dad66f544c48c
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-03-28 12:58:06 -0400

    Remove redux and redux files

--- src/js/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import socketMiddleware from './utils/socket-middleware';
-import root from './reducers';
-
-const store = createStore(root, applyMiddleware(thunk, socketMiddleware));
-
-export default store;
