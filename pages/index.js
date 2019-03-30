818f6956ea212f4ad05f883500ae05090fa8decc
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-03-30 18:33:09 -0400

    Index page now explicity renders DashboardHome component instead of just rendering Dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import LandingPage from '../components/landing-page';
-import Dashboard from '../components/dashboard';
+import DashboardHome from '../components/dashboard/dashboard-home';
 import { getOrCreate } from '../lib/services/ripple-api';
 import wrapUnauthorized from '../lib/utils/wrap-unauthorized';
 import featuredStations from '../lib/data/featured-stations.json';
 
 export default class Index extends Component {
   static async getInitialProps(context) {
+    const { query } = context;
     const ripple = await getOrCreate(context);
 
     const results = await Promise.all([
@@ -16,35 +17,22 @@ export default class Index extends Component {
     ]);
 
     return {
-      stations: results[0] ? results[0].stations : [],
+      popularStations: results[0] ? results[0].stations : [],
       user: results[1],
       followingStations: results[2] ? results[2].stations : [],
       currentPath: context.pathname,
       featuredStation:
-        featuredStations[parseInt(Math.random() * featuredStations.length, 10)]
+        featuredStations[parseInt(Math.random() * featuredStations.length, 10)],
+      query
     };
   }
 
   render() {
-    const {
-      user,
-      stations,
-      followingStations,
-      featuredStation,
-      currentPath
-    } = this.props;
+    const { user, popularStations, query } = this.props;
 
     if (user) {
-      return (
-        <Dashboard
-          user={user}
-          popularStations={stations}
-          followingStations={followingStations}
-          currentPath={currentPath}
-          featuredStation={featuredStation}
-        />
-      );
+      return <DashboardHome {...this.props} />;
     }
-    return <LandingPage stations={stations} />;
+    return <LandingPage stations={popularStations} query={query} />;
   }
 }
